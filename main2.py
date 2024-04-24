d63ca66edda7375d21a7da0d2ddc64356c0e5f20
Пин Дунь Дунь <151181242+vovkan0303@users.noreply.github.com>
2024-04-24 16:27:45 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1185,7 +1185,7 @@ def reqister():
 
 def main():
     db_session.global_init("db/polzovateli.sqlite")
-    app.run(app.run(port=8080, host='127.0.0.1'))
+    app.run(debug=False, host='0.0.0.0')
 
 
 if __name__ == '__main__':
